861f6078503b90be760f2df754651e1eef555eb0
RinRintech <144243719+RinRintech@users.noreply.github.com>
2023-10-23 18:19:11 +0900

    Userテーブル作成
    買い物リストテーブルにおいて
    user_idカラムがnull falseのため
    先にテーブルだけ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_083421) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_091444) do
   create_table "lists", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
     t.string "title"
@@ -24,6 +24,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_083421) do
     t.datetime "updated_at", null: false
     t.index ["template_id"], name: "index_lists_on_template_id"
     t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
